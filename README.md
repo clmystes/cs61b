81357f0d17f44ed00ec1b2deea164a969724e1fc
Clmystes <clmystes@gmail.com>
2020-05-21 23:28:22 +0800

    finish maze generator

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 ## Resouce
 
-https://sp18.datastructur.es/
+- 课程主页: https://sp18.datastructur.es/
+- 迷宫生成算法: https://zhuanlan.zhihu.com/p/27381213
--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,18 +1,96 @@
+// Recursive division method
+
 package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.LinkedList;
 
 public class Game {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+    public static final TETile WALL = Tileset.WALL;
+    public static final TETile FLOOR = Tileset.FLOOR;
+    public static final TETile LOCKED_DOOR = Tileset.LOCKED_DOOR;
+    public static final TETile NOTHING = Tileset.NOTHING;
+
+    private TETile[][] world;
+    private LinkedList<Position> list;
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
     public void playWithKeyboard() {
+        initWorld();
+        fillWorld();
+        drawWorld();
+    }
+
+    private void drawWorld() {
+        ter.renderFrame(world);
+    }
+
+    private void initWorld() {
+        ter.initialize(WIDTH, HEIGHT);
+        world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                if(x == 0 || y == 0 || x == (WIDTH - 1) || y == (HEIGHT - 1)) {
+                    world[x][y] = WALL;
+                } else {
+                    world[x][y] = FLOOR;
+                }
+            }
+        }
+    }
+
+    private void fillWorld() {
+        divide(new Position(1, 1), new Position(WIDTH - 2, HEIGHT - 2));
+    }
+
+    public static int getRandom(int min, int max) {
+        return (int) (Math.random() * (max + 1 - min)) + min;
+    }
+
+    private void divide(Position left, Position right) {
+        if(right.y - left.y < 1 || right.x - left.x < 1) {
+            return;
+        }
+
+        int y = getRandom(left.y+1, right.y-1);
+        int x = getRandom(left.x+1, right.x-1);
+
+        // generate wall
+        for (int i = left.x; i <= right.x; i++) {
+           world[i][y] = WALL;
+        }
+        for (int i = left.y; i <= right.y; i++) {
+            world[x][i] = WALL;
+        }
+        // generate hole in wall
+        int holeIndex =  getRandom(1, 4);
+        if(holeIndex != 1) {
+            world[x][getRandom(left.y+1, y-1)] = FLOOR;
+        }
+        if(holeIndex != 2) {
+            world[x][getRandom(y+1, right.y-1)] = FLOOR;
+        }
+        if(holeIndex != 3) {
+            world[getRandom(left.x+1, x-1)][y] = FLOOR;
+        }
+        if(holeIndex != 4) {
+            world[getRandom(x+1, right.x-1)][y] = FLOOR;
+        }
+
+        divide(new Position(left.x, left.y), new Position(x-1, y-1));
+        divide(new Position(x+1, y+1), new Position(right.x, right.y));
+
+        divide(new Position(left.x, y+1), new Position(x-1, right.y));
+        divide(new Position(x+1, left.y), new Position(right.x, y-1));
     }
 
     /**
